5a985601c80d31e60d16a29b99305fa3c7cb7ead
Dave Banerjee <dvbnrg@gmail.com>
2019-07-13 11:49:39 -0500

    git init @ Tour of Heroes part 3:Master Detail Components

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: '1', name: 'Wolverine' },
+  { id: '2', name: 'Jean Gray' },
+  { id: '3', name: 'Cyclops' },
+  { id: '4', name: 'Storm' },
+  { id: '5', name: 'Nightcrawler' },
+  { id: '6', name: 'Proffesor X' },
+  { id: '7', name: 'Rogue' },
+  { id: '8', name: 'Magneto' },
+  { id: '9', name: 'Mystique' },
+  { id: '10', name: 'Quicksilver' },
+];
